fa55c0d4fb320d36570f3fe0331f5c8635d09aea
Jiaming Guo <j23guo@gmail.com>
2022-07-28 23:34:31 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Jiaming Guo on 2022-07-28.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
